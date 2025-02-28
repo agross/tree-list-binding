2714b5200aa4aa0d9dc7601014027d8f330c3373
Alexander Groß <agross@therightstuff.de>
2025-02-28 10:24:57 +0100

    Tree ist leer

---
+++ Tests/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Shouldly;
+
+using Xunit;
+
+namespace Tests
+{
+  public class TreeTests
+  {
+    [Fact]
+    public void ist_anfangs_leer()
+    {
+      var tree = new Tree();
+
+      tree.Nodes.ShouldBeEmpty();
+    }
+  }
+
+  public class Tree
+  {
+    public IEnumerable<object> Nodes { get; } = new List<object>();
+  }
+}
---
+++ Tree/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using Xunit;
+
+namespace Tree
+{
+  public class TreeTests
+  {
+    [Fact]
+    public void ist_anfangs_leer()
+    {
+      var tree = new Tree();
+
+      tree.Nodes.ShouldBeEmpty();
+    }
+  }
+}
